13db3bad8b18bb114ed7972ccf77246e1af22d08
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2024-01-31 12:27:27 +0800

    Optimize.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Media.Animation;
 using System.Windows.Media;
-using System.Windows.Controls;
 
 namespace Lottery
 {
