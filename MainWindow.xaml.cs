834baf889a67bd2d3d62343fec2105f61a7a72f6
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2024-02-25 20:39:46 +0800

    Update MainWindow.xaml.cs.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Media;
-using System.Diagnostics;
 using System.Numerics;
 
 namespace Lottery
@@ -31,7 +30,7 @@ namespace Lottery
             Ani.TextBoxBind(quat);
         }
 
-        private BigInteger NextBigInteger(Random random, BigInteger minValue, BigInteger maxValue)
+        public BigInteger NextBigInteger(Random random, BigInteger minValue, BigInteger maxValue)
         {
             if (minValue == maxValue) return minValue;
             BigInteger zeroBasedUpperBound = maxValue - minValue;
